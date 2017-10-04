cd54ae8aff8a2b8a6f0315f1b0e4a8958430461b
george <george@georges-macbook-pro-2.local>
2017-10-04 10:23:17 +1000

    Initial Commit

---
+++ CombFilter Mic1a/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CombFilter Mic1a
+//
+//  Created by george on 10/4/17.
+//  Copyright © 2017 george. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
